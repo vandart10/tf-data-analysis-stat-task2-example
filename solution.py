11a62def5283f496bb23e8128aaccfd262a99fcd
vandart10 <73397775+vandart10@users.noreply.github.com>
2023-04-06 10:54:00 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -10,9 +10,10 @@ def solution(p: float, x: np.array) -> tuple:
     N = len(x)
     T_2 = 77*77 / 2
     mean = np.mean(x)
-    er = stats.erlang(N, 1)
-    g1 = er.ppf((1-p)/2)/N
-    g2 = er.ppf((1+p)/2)/N
-    l = (1*g1+g1 + mean-1/2) / T_2
-    r = (1*g1+g2 + mean-1/2) / T_2
+
+    g1 = stats.erlang.ppf(p / 2, N, loc=0, scale=1/N)
+    g2 = stats.erlang.ppf(1 - p / 2, N, loc=0, scale=1/N)
+
+    l = (g1 + mean - 1/2) / T_2
+    r = (g2 + mean - 1/2) / T_2
     return l, r
